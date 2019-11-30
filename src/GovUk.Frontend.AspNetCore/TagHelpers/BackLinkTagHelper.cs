4e59f741c031ac35efb3b1dc2780b106a906c584
James Gunn <james@gunn.io>
2019-11-30 13:30:15 +0000

    Add BackLinkTagHelper (closes #6)

---
+++ src/GovUk.Frontend.AspNetCore/TagHelpers/BackLinkTagHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace GovUk.AspNetCore.TagHelpers
+{
+    [HtmlTargetElement("govuk-back-link", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class BackLinkTagHelper : AnchorTagHelper
+    {
+        public BackLinkTagHelper(IHtmlGenerator generator)
+            : base(generator)
+        {
+        }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            await base.ProcessAsync(context, output);
+
+            if (output.Content.IsEmptyOrWhiteSpace)
+            {
+                output.Content.Append("Back");
+            }
+
+            output.AddClass("govuk-back-link", HtmlEncoder.Default);
+        }
+    }
+}
