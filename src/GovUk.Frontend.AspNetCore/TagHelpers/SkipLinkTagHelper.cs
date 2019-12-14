81bd77f5a0aba8b5d1d4ece52525683fca4352a0
James Gunn <james@gunn.io>
2019-12-14 11:27:59 +0000

    Add SkipLinkTagHelper (closes #27)

---
+++ src/GovUk.Frontend.AspNetCore/TagHelpers/SkipLinkTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace GovUk.Frontend.AspNetCore.TagHelpers
+{
+    [HtmlTargetElement("govuk-skip-link", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class SkipLinkTagHelper : AnchorTagHelper
+    {
+        public SkipLinkTagHelper(IHtmlGenerator generator)
+            : base(generator)
+        {
+        }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            await base.ProcessAsync(context, output);
+
+            output.AddClass("govuk-skip-link", HtmlEncoder.Default);
+        }
+    }
+}
---
+++ test/GovUk.Frontend.AspNetCore.Tests/TagHelpers/SkipLinkTagHelperTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using GovUk.Frontend.AspNetCore.TagHelpers;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Moq;
+using Xunit;
+
+namespace GovUk.Frontend.AspNetCore.Tests.TagHelpers
+{
+    public class SkipLinkTagHelperTests
+    {
+        [Fact]
+        public async Task ProcessAsync_WithContentGeneratesExpectedOutput()
+        {
+            // Arrange
+            var context = new TagHelperContext(
+                tagName: "govuk-skip-link",
+                allAttributes: new TagHelperAttributeList(),
+                items: new Dictionary<object, object>(),
+                uniqueId: "test");
+
+            var output = new TagHelperOutput(
+                "govuk-skip-link",
+                attributes: new TagHelperAttributeList()
+                {
+                    { "href", "http://foo.com" }
+                },
+                getChildContentAsync: (useCachedResult, encoder) =>
+                {
+                    var tagHelperContent = new DefaultTagHelperContent();
+                    tagHelperContent.SetContent("My custom link content");
+                    return Task.FromResult<TagHelperContent>(tagHelperContent);
+                });
+            output.Content.SetContent("My custom link content");
+
+            var htmlGenerator = new Mock<IHtmlGenerator>();
+            htmlGenerator
+                .Setup(
+                    mock => mock.GenerateActionLink(
+                        /*viewContext: */It.IsAny<ViewContext>(),
+                        /*linkText: */It.IsAny<string>(),
+                        /*actionName: */It.IsAny<string>(),
+                        /*controllerName: */It.IsAny<string>(),
+                        /*protocol: */It.IsAny<string>(),
+                        /*hostname: */It.IsAny<string>(),
+                        /*fragment: */It.IsAny<string>(),
+                        /*routeValues: */It.IsAny<IDictionary<string, object>>(),
+                        /*htmlAttributes: */It.IsAny<object>()))
+                .Returns(new TagBuilder("a"));
+
+            var tagHelper = new SkipLinkTagHelper(htmlGenerator.Object);
+
+            // Act
+            await tagHelper.ProcessAsync(context, output);
+
+            // Assert
+            Assert.Equal("a", output.TagName);
+            Assert.Equal(TagMode.StartTagAndEndTag, output.TagMode);
+            Assert.Equal("http://foo.com", output.Attributes["href"].Value);
+            Assert.Equal("govuk-skip-link", output.Attributes["class"].Value);
+            Assert.Equal("My custom link content", output.Content.GetContent());
+        }
+    }
+}
