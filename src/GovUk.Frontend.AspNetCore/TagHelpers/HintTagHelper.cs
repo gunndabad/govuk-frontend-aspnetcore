36b467bcf77640052c755af698f7573028ca98bf
James Gunn <james@gunn.io>
2019-12-14 12:25:48 +0000

    Add HintTagHelper (closes #19)

---
+++ src/GovUk.Frontend.AspNetCore/TagHelpers/HintTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace GovUk.Frontend.AspNetCore.TagHelpers
+{
+    [HtmlTargetElement("govuk-hint", TagStructure = TagStructure.Unspecified)]
+    public class HintTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "span";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.AddClass("govuk-hint", HtmlEncoder.Default);
+
+            var childContent = await output.GetChildContentAsync();
+            output.Content.AppendHtml(childContent);
+        }
+    }
+}
---
+++ test/GovUk.Frontend.AspNetCore.Tests/TagHelpers/HintTagHelperTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using GovUk.Frontend.AspNetCore.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Xunit;
+
+namespace GovUk.Frontend.AspNetCore.Tests.TagHelpers
+{
+    public class HintTagHelperTests
+    {
+        [Fact]
+        public async Task ProcessAsync_WithContentGeneratesExpectedOutput()
+        {
+            // Arrange
+            var context = new TagHelperContext(
+                tagName: "govuk-hint",
+                allAttributes: new TagHelperAttributeList(),
+                items: new Dictionary<object, object>(),
+                uniqueId: "test");
+
+            var output = new TagHelperOutput(
+                "govuk-hint",
+                attributes: new TagHelperAttributeList(),
+                getChildContentAsync: (useCachedResult, encoder) =>
+                {
+                    var tagHelperContent = new DefaultTagHelperContent();
+                    tagHelperContent.SetContent("Hint text");
+                    return Task.FromResult<TagHelperContent>(tagHelperContent);
+                });
+
+            var tagHelper = new HintTagHelper();
+
+            // Act
+            await tagHelper.ProcessAsync(context, output);
+
+            // Assert
+            Assert.Equal("span", output.TagName);
+            Assert.Equal(TagMode.StartTagAndEndTag, output.TagMode);
+            Assert.Equal("govuk-hint", output.Attributes["class"].Value);
+            Assert.Equal("Hint text", output.Content.GetContent());
+        }
+    }
+}
