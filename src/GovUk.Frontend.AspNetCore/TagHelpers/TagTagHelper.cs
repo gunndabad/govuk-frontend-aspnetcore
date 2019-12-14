ec8abf2482265f414d34790166c21b0f1622da80
James Gunn <james@gunn.io>
2019-12-14 11:48:28 +0000

    Add TagTagHelper (closes #31)

---
+++ src/GovUk.Frontend.AspNetCore/TagHelpers/TagTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace GovUk.Frontend.AspNetCore.TagHelpers
+{
+    [HtmlTargetElement("govuk-tag", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class TagTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "strong";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.AddClass("govuk-tag", HtmlEncoder.Default);
+
+            var childContent = await output.GetChildContentAsync();
+            output.Content.AppendHtml(childContent);
+        }
+    }
+}
---
+++ test/GovUk.Frontend.AspNetCore.Tests/TagHelpers/TagTagHelperTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using GovUk.Frontend.AspNetCore.TagHelpers;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Moq;
+using Xunit;
+
+namespace GovUk.Frontend.AspNetCore.Tests.TagHelpers
+{
+    public class TagTagHelperTests
+    {
+        [Fact]
+        public async Task ProcessAsync_WithContentGeneratesExpectedOutput()
+        {
+            // Arrange
+            var context = new TagHelperContext(
+                tagName: "govuk-tag",
+                allAttributes: new TagHelperAttributeList(),
+                items: new Dictionary<object, object>(),
+                uniqueId: "test");
+
+            var output = new TagHelperOutput(
+                "govuk-tag",
+                attributes: new TagHelperAttributeList(),
+                getChildContentAsync: (useCachedResult, encoder) =>
+                {
+                    var tagHelperContent = new DefaultTagHelperContent();
+                    tagHelperContent.SetContent("A tag");
+                    return Task.FromResult<TagHelperContent>(tagHelperContent);
+                });
+
+            var tagHelper = new TagTagHelper();
+
+            // Act
+            await tagHelper.ProcessAsync(context, output);
+
+            // Assert
+            Assert.Equal("strong", output.TagName);
+            Assert.Equal(TagMode.StartTagAndEndTag, output.TagMode);
+            Assert.Equal("govuk-tag", output.Attributes["class"].Value);
+            Assert.Equal("A tag", output.Content.GetContent());
+        }
+    }
+}
