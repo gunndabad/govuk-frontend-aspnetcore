8c9c78f2af76a3db471d9cb9c6b4d8488a5c8414
James Gunn <james@gunn.io>
2019-12-21 14:12:32 +0000

    Add LabelTagHelper (closes #22)

---
+++ src/GovUk.Frontend.AspNetCore/TagHelpers/LabelTagHelper.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace GovUk.Frontend.AspNetCore.TagHelpers
+{
+    [HtmlTargetElement("govuk-label", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class LabelTagHelper : TagHelper
+    {
+        private const string IsPageHeadingAttributeName = "is-page-heading";
+        private const string AspForAttributeName = "asp-for";
+        private const string ForAttributeName = "for";
+
+        private readonly IHtmlGenerator _htmlGenerator;
+
+        public LabelTagHelper(IHtmlGenerator htmlGenerator)
+        {
+            _htmlGenerator = htmlGenerator;
+        }
+
+        [HtmlAttributeName(AspForAttributeName)]
+        public ModelExpression AspFor { get; set; }
+
+        [HtmlAttributeName(ForAttributeName)]
+        public string For { get; set; }
+
+        [HtmlAttributeName(IsPageHeadingAttributeName)]
+        public bool IsPageHeading { get; set; }
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (AspFor != null && For != null)
+            {
+                throw new InvalidOperationException($"Cannot specify both the '{AspForAttributeName}' and '{ForAttributeName}' attributes.");
+            }
+
+            output.TagName = "label";
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            var childContent = await output.GetChildContentAsync();
+
+            IHtmlContent content = childContent;
+            string @for = For;
+
+            if (AspFor != null)
+            {
+                var label = _htmlGenerator.GenerateLabel(
+                    ViewContext,
+                    AspFor.ModelExplorer,
+                    AspFor.Name,
+                    labelText: null,
+                    htmlAttributes: null);
+
+                if (childContent.IsEmptyOrWhiteSpace)
+                {
+                    content = label.InnerHtml;
+                }
+
+                if (For == null)
+                {
+                    @for = label.Attributes["for"];
+                }
+            }
+
+            output.AddClass("govuk-label", HtmlEncoder.Default);
+            output.Attributes.Add("for", @for);
+
+            if (IsPageHeading)
+            {
+                var header = new TagBuilder("h1");
+                header.AddCssClass("govuk-label-wrapper");
+
+                output.PreElement.AppendHtml(header.RenderStartTag());
+                output.PostElement.AppendHtml(header.RenderEndTag());
+            }
+
+            output.Content.AppendHtml(content);
+        }
+    }
+}
---
+++ test/GovUk.Frontend.AspNetCore.Tests/TagHelpers/LabelTagHelperTests.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GovUk.Frontend.AspNetCore.TagHelpers;
+using Microsoft.AspNetCore.Mvc.Internal;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Moq;
+using Xunit;
+
+namespace GovUk.Frontend.AspNetCore.Tests.TagHelpers
+{
+    public class LabelTagHelperTests
+    {
+        [Fact]
+        public async Task ProcessAsync_WithContentAndForAttributeGeneratesExpectedOutput()
+        {
+            // Arrange
+            var context = new TagHelperContext(
+                tagName: "govuk-label",
+                allAttributes: new TagHelperAttributeList(),
+                items: new Dictionary<object, object>(),
+                uniqueId: "test");
+
+            var output = new TagHelperOutput(
+                "govuk-label",
+                attributes: new TagHelperAttributeList(),
+                getChildContentAsync: (useCachedResult, encoder) =>
+                {
+                    var tagHelperContent = new DefaultTagHelperContent();
+                    tagHelperContent.SetContent("Label content");
+                    return Task.FromResult<TagHelperContent>(tagHelperContent);
+                });
+
+            var htmlGenerator = new Mock<IHtmlGenerator>();
+            htmlGenerator
+                .Setup(
+                    mock => mock.GenerateLabel(
+                        /*viewContext: */It.IsAny<ViewContext>(),
+                        /*modelExplorer: */It.IsAny<ModelExplorer>(),
+                        /*expression: */It.IsAny<string>(),
+                        /*labelText: */It.IsAny<string>(),
+                        /*htmlAttributes: */It.IsAny<object>()))
+                .Returns(new TagBuilder("label"));
+
+            var tagHelper = new LabelTagHelper(htmlGenerator.Object)
+            {
+                For = "some-input-id"
+            };
+
+            // Act
+            await tagHelper.ProcessAsync(context, output);
+
+            // Assert
+            Assert.Equal("label", output.TagName);
+            Assert.Equal(TagMode.StartTagAndEndTag, output.TagMode);
+            Assert.Equal("govuk-label", output.Attributes["class"].Value);
+            Assert.Equal("some-input-id", output.Attributes["for"].Value);
+            Assert.Equal("Label content", output.Content.GetContent());
+        }
+
+        [Fact]
+        public async Task ProcessAsync_WithNoContentAndAspForAttributeGeneratesExpectedOutput()
+        {
+            // Arrange
+            var context = new TagHelperContext(
+                tagName: "govuk-label",
+                allAttributes: new TagHelperAttributeList(),
+                items: new Dictionary<object, object>(),
+                uniqueId: "test");
+
+            var output = new TagHelperOutput(
+                "govuk-label",
+                attributes: new TagHelperAttributeList(),
+                getChildContentAsync: (useCachedResult, encoder) =>
+                {
+                    var tagHelperContent = new DefaultTagHelperContent();
+                    return Task.FromResult<TagHelperContent>(tagHelperContent);
+                });
+
+            var htmlGenerator = new Mock<IHtmlGenerator>();
+            htmlGenerator
+                .Setup(
+                    mock => mock.GenerateLabel(
+                        /*viewContext: */It.IsAny<ViewContext>(),
+                        /*modelExplorer: */It.IsAny<ModelExplorer>(),
+                        /*expression: */It.IsAny<string>(),
+                        /*labelText: */It.IsAny<string>(),
+                        /*htmlAttributes: */It.IsAny<object>()))
+                .Returns(() =>
+                {
+                    var tagBuilder = new TagBuilder("label");
+                    tagBuilder.Attributes.Add("for", "Foo");
+                    tagBuilder.InnerHtml.Append("Generated label");
+                    return tagBuilder;
+                });
+
+            var modelExplorer = new EmptyModelMetadataProvider().GetModelExplorerForType(typeof(Model), "Foo");
+
+            var tagHelper = new LabelTagHelper(htmlGenerator.Object)
+            {
+                AspFor = new ModelExpression("Foo", modelExplorer)
+            };
+
+            // Act
+            await tagHelper.ProcessAsync(context, output);
+
+            // Assert
+            Assert.Equal("label", output.TagName);
+            Assert.Equal(TagMode.StartTagAndEndTag, output.TagMode);
+            Assert.Equal("govuk-label", output.Attributes["class"].Value);
+            Assert.Equal("Foo", output.Attributes["for"].Value);
+            Assert.Equal("Generated label", output.Content.GetContent());
+        }
+
+        [Fact]
+        public async Task ProcessAsync_WithContentAndAspForAttributeGeneratesExpectedOutput()
+        {
+            // Arrange
+            var context = new TagHelperContext(
+                tagName: "govuk-label",
+                allAttributes: new TagHelperAttributeList(),
+                items: new Dictionary<object, object>(),
+                uniqueId: "test");
+
+            var output = new TagHelperOutput(
+                "govuk-label",
+                attributes: new TagHelperAttributeList(),
+                getChildContentAsync: (useCachedResult, encoder) =>
+                {
+                    var tagHelperContent = new DefaultTagHelperContent();
+                    tagHelperContent.SetContent("Specific content");
+                    return Task.FromResult<TagHelperContent>(tagHelperContent);
+                });
+
+            var htmlGenerator = new Mock<IHtmlGenerator>();
+            htmlGenerator
+                .Setup(
+                    mock => mock.GenerateLabel(
+                        /*viewContext: */It.IsAny<ViewContext>(),
+                        /*modelExplorer: */It.IsAny<ModelExplorer>(),
+                        /*expression: */It.IsAny<string>(),
+                        /*labelText: */It.IsAny<string>(),
+                        /*htmlAttributes: */It.IsAny<object>()))
+                .Returns(() =>
+                {
+                    var tagBuilder = new TagBuilder("label");
+                    tagBuilder.Attributes.Add("for", "Foo");
+                    tagBuilder.InnerHtml.Append("Generated label");
+                    return tagBuilder;
+                });
+
+            var modelExplorer = new EmptyModelMetadataProvider().GetModelExplorerForType(typeof(Model), "Foo");
+
+            var tagHelper = new LabelTagHelper(htmlGenerator.Object)
+            {
+                AspFor = new ModelExpression("Foo", modelExplorer)
+            };
+
+            // Act
+            await tagHelper.ProcessAsync(context, output);
+
+            // Assert
+            Assert.Equal("label", output.TagName);
+            Assert.Equal(TagMode.StartTagAndEndTag, output.TagMode);
+            Assert.Equal("govuk-label", output.Attributes["class"].Value);
+            Assert.Equal("Foo", output.Attributes["for"].Value);
+            Assert.Equal("Specific content", output.Content.GetContent());
+        }
+
+        [Fact]
+        public async Task ProcessAsync_IsPageHeadingSpecifiedGeneratesExpectedOutput()
+        {
+            // Arrange
+            var context = new TagHelperContext(
+                tagName: "govuk-label",
+                allAttributes: new TagHelperAttributeList(),
+                items: new Dictionary<object, object>(),
+                uniqueId: "test");
+
+            var output = new TagHelperOutput(
+                "govuk-label",
+                attributes: new TagHelperAttributeList(),
+                getChildContentAsync: (useCachedResult, encoder) =>
+                {
+                    var tagHelperContent = new DefaultTagHelperContent();
+                    tagHelperContent.SetContent("Label content");
+                    return Task.FromResult<TagHelperContent>(tagHelperContent);
+                });
+
+            var htmlGenerator = new Mock<IHtmlGenerator>();
+            htmlGenerator
+                .Setup(
+                    mock => mock.GenerateLabel(
+                        /*viewContext: */It.IsAny<ViewContext>(),
+                        /*modelExplorer: */It.IsAny<ModelExplorer>(),
+                        /*expression: */It.IsAny<string>(),
+                        /*labelText: */It.IsAny<string>(),
+                        /*htmlAttributes: */It.IsAny<object>()))
+                .Returns(new TagBuilder("label"));
+
+            var tagHelper = new LabelTagHelper(htmlGenerator.Object)
+            {
+                For = "some-input-id",
+                IsPageHeading = true
+            };
+
+            // Act
+            await tagHelper.ProcessAsync(context, output);
+
+            // Assert
+            Assert.Equal("<h1 class=\"govuk-label-wrapper\">", output.PreElement.GetContent());
+            Assert.Equal("</h1>", output.PostElement.GetContent());
+        }
+
+        [Fact]
+        public async Task ProcessAsync_BothAspForAndForSpecifiedThrowsInvalidOperationException()
+        {
+            // Arrange
+            var context = new TagHelperContext(
+                tagName: "govuk-label",
+                allAttributes: new TagHelperAttributeList(),
+                items: new Dictionary<object, object>(),
+                uniqueId: "test");
+
+            var output = new TagHelperOutput(
+                "govuk-label",
+                attributes: new TagHelperAttributeList(),
+                getChildContentAsync: (useCachedResult, encoder) =>
+                {
+                    var tagHelperContent = new DefaultTagHelperContent();
+                    return Task.FromResult<TagHelperContent>(tagHelperContent);
+                });
+
+            var htmlGenerator = new Mock<IHtmlGenerator>();
+
+            var modelExplorer = new EmptyModelMetadataProvider().GetModelExplorerForType(typeof(Model), "Foo");
+
+            var tagHelper = new LabelTagHelper(htmlGenerator.Object)
+            {
+                AspFor = new ModelExpression("Foo", modelExplorer),
+                For = "some-input-id"
+            };
+
+            // Act & Assert
+            var ex = await Assert.ThrowsAsync<InvalidOperationException>(() => tagHelper.ProcessAsync(context, output));
+            Assert.Equal("Cannot specify both the 'asp-for' and 'for' attributes.", ex.Message);
+        }
+
+        public class Model
+        {
+            public string Foo { get; set; }
+        }
+    }
+}
