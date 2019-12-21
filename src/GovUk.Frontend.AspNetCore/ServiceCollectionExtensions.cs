ae86a4637eb38f46d92e688b53f63466668be4c6
James Gunn <james@gunn.io>
2019-12-21 15:40:43 +0000

    Add placeholder AddGovUkFrontend() method

---
+++ src/GovUk.Frontend.AspNetCore/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace GovUk.Frontend.AspNetCore
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddGovUkFrontend(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
