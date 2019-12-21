3a6560850d7105e47bf5ff11b45b89a985778b30
James Gunn <james@gunn.io>
2019-12-21 15:57:06 +0000

    Move Tag logic into generator

--- src/GovUk.Frontend.AspNetCore/TagHelpers/TagTagHelper.cs
+++ src/GovUk.Frontend.AspNetCore/TagHelpers/TagTagHelper.cs
@@ -1,4 +1,4 @@
-﻿using System.Text.Encodings.Web;
+﻿using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Microsoft.AspNetCore.Razor.TagHelpers;
@@ -8,14 +8,24 @@ namespace GovUk.Frontend.AspNetCore.TagHelpers
     [HtmlTargetElement("govuk-tag", TagStructure = TagStructure.NormalOrSelfClosing)]
     public class TagTagHelper : TagHelper
     {
+        private readonly IGovUkHtmlGenerator _htmlGenerator;
+
+        public TagTagHelper(IGovUkHtmlGenerator htmlGenerator)
+        {
+            _htmlGenerator = htmlGenerator ?? throw new ArgumentNullException(nameof(htmlGenerator));
+        }
+
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
-            output.TagName = "strong";
-            output.TagMode = TagMode.StartTagAndEndTag;
-            output.AddClass("govuk-tag", HtmlEncoder.Default);
-
             var childContent = await output.GetChildContentAsync();
-            output.Content.AppendHtml(childContent);
+
+            var tagBuilder = _htmlGenerator.GenerateTag(childContent);
+
+            output.TagName = tagBuilder.TagName;
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            output.MergeAttributes(tagBuilder);
+            output.Content.SetHtmlContent(tagBuilder.InnerHtml);
         }
     }
 }
--- test/GovUk.Frontend.AspNetCore.Tests/TagHelpers/TagTagHelperTests.cs
+++ test/GovUk.Frontend.AspNetCore.Tests/TagHelpers/TagTagHelperTests.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using GovUk.Frontend.AspNetCore.TagHelpers;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
-using Moq;
 using Xunit;
 
 namespace GovUk.Frontend.AspNetCore.Tests.TagHelpers
@@ -33,7 +28,7 @@ namespace GovUk.Frontend.AspNetCore.Tests.TagHelpers
                     return Task.FromResult<TagHelperContent>(tagHelperContent);
                 });
 
-            var tagHelper = new TagTagHelper();
+            var tagHelper = new TagTagHelper(new DefaultGovUkHtmlGenerator());
 
             // Act
             await tagHelper.ProcessAsync(context, output);
