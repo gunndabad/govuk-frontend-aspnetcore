606c9fb1848622e0bc5c99ed8d21144278e560d7
James Gunn <james@gunn.io>
2019-12-24 15:34:04 +0000

    Add base class for TagHelpers that generate links

---
+++ src/GovUk.Frontend.AspNetCore/TagHelpers/LinkTagHelperBase.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Routing;
+
+namespace GovUk.Frontend.AspNetCore.TagHelpers
+{
+    public abstract class LinkTagHelperBase : TagHelper
+    {
+        private const string ActionAttributeName = "asp-action";
+        private const string ControllerAttributeName = "asp-controller";
+        private const string AreaAttributeName = "asp-area";
+        private const string PageAttributeName = "asp-page";
+        private const string PageHandlerAttributeName = "asp-page-handler";
+        private const string FragmentAttributeName = "asp-fragment";
+        private const string HostAttributeName = "asp-host";
+        private const string ProtocolAttributeName = "asp-protocol";
+        private const string RouteAttributeName = "asp-route";
+        private const string RouteValuesDictionaryName = "asp-all-route-data";
+        private const string RouteValuesPrefix = "asp-route-";
+        private const string HrefAttributeName = "href";
+
+        private IDictionary<string, string> _routeValues;
+
+        public LinkTagHelperBase(IHtmlGenerator htmlGenerator)
+        {
+            Generator = htmlGenerator ?? throw new ArgumentNullException(nameof(htmlGenerator));
+        }
+
+        protected IHtmlGenerator Generator { get; }
+
+        [HtmlAttributeName(ActionAttributeName)]
+        public string Action { get; set; }
+
+        [HtmlAttributeName(AreaAttributeName)]
+        public string Area { get; set; }
+
+        [HtmlAttributeName(ControllerAttributeName)]
+        public string Controller { get; set; }
+
+        [HtmlAttributeName(FragmentAttributeName)]
+        public string Fragment { get; set; }
+
+        [HtmlAttributeName(HostAttributeName)]
+        public string Host { get; set; }
+
+        [HtmlAttributeName(HrefAttributeName)]
+        public string Href { get; set; }
+
+        [HtmlAttributeName(PageAttributeName)]
+        public string Page { get; set; }
+
+        [HtmlAttributeName(PageHandlerAttributeName)]
+        public string PageHandler { get; set; }
+
+        [HtmlAttributeName(ProtocolAttributeName)]
+        public string Protocol { get; set; }
+
+        [HtmlAttributeName(RouteAttributeName)]
+        public string Route { get; set; }
+
+        [HtmlAttributeName(RouteValuesDictionaryName, DictionaryAttributePrefix = RouteValuesPrefix)]
+        public IDictionary<string, string> RouteValues
+        {
+            get
+            {
+                if (_routeValues == null)
+                {
+                    _routeValues = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+                }
+
+                return _routeValues;
+            }
+            set
+            {
+                _routeValues = value;
+            }
+        }
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        protected TagBuilder CreateAnchorTagBuilder()
+        {
+            var hrefLink = Href != null;
+            var routeLink = Route != null;
+            var actionLink = Controller != null || Action != null;
+            var pageLink = Page != null || PageHandler != null;
+
+            if (new[] { hrefLink, routeLink, actionLink, pageLink }.Count(l => l) > 1)
+            {
+                throw new InvalidOperationException(
+                    $"Cannot determine the 'href' attribute for <a>. The following attributes are mutually exclusive:\n" +
+                    $"{Href}\n" +
+                    $"{RouteAttributeName}\n" +
+                    $"{ControllerAttributeName}, {ActionAttributeName}\n" +
+                    $"{PageAttributeName}, {PageHandlerAttributeName}");
+            }
+
+            RouteValueDictionary routeValues = null;
+            if (_routeValues != null && _routeValues.Count > 0)
+            {
+                routeValues = new RouteValueDictionary(_routeValues);
+            }
+
+            if (Area != null)
+            {
+                // Unconditionally replace any value from asp-route-area.
+                if (routeValues == null)
+                {
+                    routeValues = new RouteValueDictionary();
+                }
+                routeValues["area"] = Area;
+            }
+
+            TagBuilder tagBuilder;
+            if (hrefLink)
+            {
+                tagBuilder = new TagBuilder("a");
+                tagBuilder.Attributes.Add("href", Href);
+            }
+            else if (pageLink)
+            {
+                tagBuilder = Generator.GeneratePageLink(
+                    ViewContext,
+                    linkText: string.Empty,
+                    pageName: Page,
+                    pageHandler: PageHandler,
+                    protocol: Protocol,
+                    hostname: Host,
+                    fragment: Fragment,
+                    routeValues: routeValues,
+                    htmlAttributes: null);
+            }
+            else if (routeLink)
+            {
+                tagBuilder = Generator.GenerateRouteLink(
+                    ViewContext,
+                    linkText: string.Empty,
+                    routeName: Route,
+                    protocol: Protocol,
+                    hostName: Host,
+                    fragment: Fragment,
+                    routeValues: routeValues,
+                    htmlAttributes: null);
+            }
+            else
+            {
+                tagBuilder = Generator.GenerateActionLink(
+                   ViewContext,
+                   linkText: string.Empty,
+                   actionName: Action,
+                   controllerName: Controller,
+                   protocol: Protocol,
+                   hostname: Host,
+                   fragment: Fragment,
+                   routeValues: routeValues,
+                   htmlAttributes: null);
+            }
+
+            return tagBuilder;
+        }
+    }
+}
