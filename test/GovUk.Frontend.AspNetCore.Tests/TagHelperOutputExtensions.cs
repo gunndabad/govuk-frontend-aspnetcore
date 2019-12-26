0f182a249e2cfb92f5be17cafb2e4f08c7c73324
James Gunn <james@gunn.io>
2019-12-26 11:49:10 +0000

    Helper method for retrieving tag helper's output as HTML

---
+++ test/GovUk.Frontend.AspNetCore.Tests/TagHelperOutputExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace GovUk.Frontend.AspNetCore.Tests
+{
+    public static class TagHelperOutputExtensions
+    {
+        public static string AsString(this TagHelperOutput output)
+        {
+            var writer = new StringWriter();
+            output.WriteTo(writer, HtmlEncoder.Default);
+            return writer.ToString();
+        }
+    }
+}
