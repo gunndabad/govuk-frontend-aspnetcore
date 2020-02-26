9c6be11419f468bec8d5ae92c3c664c9e3bc4186
James Gunn <james@gunn.io>
2020-02-26 18:49:41 +0000

    Add extension method for cloning TagHelperContents

---
+++ src/GovUk.Frontend.AspNetCore/TagHelperContentExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace GovUk.Frontend.AspNetCore
+{
+    internal static class TagHelperContentExtensions
+    {
+        public static IHtmlContent Snapshot(this TagHelperContent content)
+        {
+            if (content == null)
+            {
+                throw new ArgumentNullException(nameof(content));
+            }
+
+            return new HtmlString(content.GetContent());
+        }
+    }
+}
