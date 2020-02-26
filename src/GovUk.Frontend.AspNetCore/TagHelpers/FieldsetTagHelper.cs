1e451aa300a40c222b856d824fb027ae6bddeff0
James Gunn <james@gunn.io>
2020-02-26 11:06:31 +0000

    Add FieldsetTagHelper (closes #15)

---
+++ src/GovUk.Frontend.AspNetCore/TagHelpers/FieldsetTagHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace GovUk.Frontend.AspNetCore.TagHelpers
+{
+    [HtmlTargetElement("govuk-fieldset", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class FieldsetTagHelper : TagHelper
+    {
+        private const string DescribedByAttributeName = "described-by";
+        private const string IsPageHeadingAttributeName = "is-page-heading";
+        private const string RoleAttributeName = "role";
+
+        private readonly IGovUkHtmlGenerator _htmlGenerator;
+
+        public FieldsetTagHelper(IGovUkHtmlGenerator htmlGenerator)
+        {
+            _htmlGenerator = htmlGenerator;
+        }
+
+        [HtmlAttributeName(DescribedByAttributeName)]
+        public string DescribedBy { get; set; }
+
+        [HtmlAttributeName(IsPageHeadingAttributeName)]
+        public bool IsPageHeading { get; set; }
+
+        [HtmlAttributeName(RoleAttributeName)]
+        public string Role { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var childContent = await output.GetChildContentAsync();
+
+            var tagBuilder = _htmlGenerator.GenerateFieldset(DescribedBy, IsPageHeading, Role, childContent);
+
+            output.TagName = tagBuilder.TagName;
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            output.MergeAttributes(tagBuilder);
+            output.Content.SetHtmlContent(tagBuilder.InnerHtml);
+        }
+    }
+}
