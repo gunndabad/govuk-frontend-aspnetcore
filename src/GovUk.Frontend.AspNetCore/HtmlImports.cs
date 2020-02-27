0163d167344c74203775238b70b427be1ac988cb
James Gunn <james@gunn.io>
2020-02-27 11:40:17 +0000

    Add import snippets to public API so they be included manually if required

---
+++ src/GovUk.Frontend.AspNetCore/HtmlImports.cs
@@ -0,0 +1,15 @@
+﻿namespace GovUk.Frontend.AspNetCore
+{
+    public static class HtmlSnippets
+    {
+        public const string ScriptImports = @"<script src=""/govuk-frontend-3.5.0.min.js""></script>
+<script>window.GOVUKFrontend.initAll()</script>";
+
+        public const string StyleImports = @"<!--[if !IE 8]><!-->
+    <link rel=""stylesheet"" href=""/govuk-frontend-3.5.0.min.css"">
+<!--<![endif]-->
+<!--[if IE 8]>
+    <link rel = ""stylesheet"" href=""/govuk-frontend-ie8-3.5.0.min.css"">
+<![endif]-->";
+    }
+}
--- src/GovUk.Frontend.AspNetCore/TagHelperComponents/GdsImportsTagHelperComponent.cs
+++ src/GovUk.Frontend.AspNetCore/TagHelperComponents/GdsImportsTagHelperComponent.cs
@@ -8,31 +8,19 @@ namespace GovUk.Frontend.AspNetCore.TagHelperComponents
 {
     public class GdsImportsTagHelperComponent : TagHelperComponent
     {
-        private const string StyleImports = @"
-<!--[if !IE 8]><!-->
-    <link rel=""stylesheet"" href=""/govuk-frontend-3.5.0.min.css"">
-<!--<![endif]-->
-<!--[if IE 8]>
-    <link rel = ""stylesheet"" href=""/govuk-frontend-ie8-3.5.0.min.css"">
-<![endif]-->";
-
-        private const string ScriptImports = @"
-<script src=""/govuk-frontend-3.5.0.min.js""></script>
-<script>window.GOVUKFrontend.initAll()</script>";
-
         public override int Order => 1;
 
         public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
             if (string.Equals(context.TagName, "head", StringComparison.OrdinalIgnoreCase))
             {
-                output.PostContent.AppendHtml(StyleImports);
+                output.PostContent.AppendHtml(HtmlSnippets.StyleImports);
             }
             else if (string.Equals(context.TagName, "body", StringComparison.OrdinalIgnoreCase))
             {
                 output.AddClass("govuk-template__body", HtmlEncoder.Default);
 
-                output.PostContent.AppendHtml(ScriptImports);
+                output.PostContent.AppendHtml(HtmlSnippets.ScriptImports);
             }
 
             return Task.CompletedTask;
