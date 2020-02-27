7e2f5a3e3d9b5824b5ffb3df473edde0552d3b28
James Gunn <james@gunn.io>
2020-02-27 10:08:19 +0000

    Add GDS frontend package and method to make it available via StaticFiles

---
+++ src/GovUk.Frontend.AspNetCore/ApplicationBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.FileProviders;
+
+namespace GovUk.Frontend.AspNetCore
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseGdsFrontEnd(this IApplicationBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            return builder.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new ManifestEmbeddedFileProvider(
+                    typeof(ApplicationBuilderExtensions).Assembly,
+                    root: "Content"),
+                RequestPath = ""  // important - CSS assumes things are available at the root
+            });
+        }
+    }
+}
