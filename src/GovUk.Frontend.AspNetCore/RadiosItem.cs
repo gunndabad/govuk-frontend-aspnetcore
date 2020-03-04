a2f766ce7de75f42a7332e9b75cd61b0fe4608b2
James Gunn <james@gunn.io>
2020-03-04 09:22:23 +0000

    Remove Name from RadiosItem

--- src/GovUk.Frontend.AspNetCore/RadiosItem.cs
+++ src/GovUk.Frontend.AspNetCore/RadiosItem.cs
@@ -10,7 +10,6 @@ namespace GovUk.Frontend.AspNetCore
     public class RadiosItem : RadiosItemBase
     {
         public string Id { get; set; }
-        public string Name { get; set; }
         public string Value { get; set; }
         public bool Checked { get; set; }
         public bool Disabled { get; set; }
