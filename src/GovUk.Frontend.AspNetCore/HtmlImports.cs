106504776d5fc57aa44e81cc4c9aaecfe51e526c
James Gunn <james@gunn.io>
2020-03-13 07:51:22 +0000

    Update GDS assets to 3.6.0

--- src/GovUk.Frontend.AspNetCore/HtmlImports.cs
+++ src/GovUk.Frontend.AspNetCore/HtmlImports.cs
@@ -2,14 +2,14 @@
 {
     public static class HtmlSnippets
     {
-        public const string ScriptImports = @"<script src=""/govuk-frontend-3.5.0.min.js""></script>
+        public const string ScriptImports = @"<script src=""/govuk-frontend-3.6.0.min.js""></script>
 <script>window.GOVUKFrontend.initAll()</script>";
 
         public const string StyleImports = @"<!--[if !IE 8]><!-->
-    <link rel=""stylesheet"" href=""/govuk-frontend-3.5.0.min.css"">
+    <link rel=""stylesheet"" href=""/govuk-frontend-3.6.0.min.css"">
 <!--<![endif]-->
 <!--[if IE 8]>
-    <link rel = ""stylesheet"" href=""/govuk-frontend-ie8-3.5.0.min.css"">
+    <link rel = ""stylesheet"" href=""/govuk-frontend-ie8-3.6.0.min.css"">
 <![endif]-->";
     }
 }
