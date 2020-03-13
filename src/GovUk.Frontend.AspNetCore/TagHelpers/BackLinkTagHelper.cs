d1fbbde5cd414e8be80c1d37522fe73c7557ddca
James Gunn <james@gunn.io>
2020-03-13 08:24:28 +0000

    Move BackLink tag creation into generator & add attributes

--- src/GovUk.Frontend.AspNetCore/TagHelpers/BackLinkTagHelper.cs
+++ src/GovUk.Frontend.AspNetCore/TagHelpers/BackLinkTagHelper.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.Encodings.Web;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Microsoft.AspNetCore.Razor.TagHelpers;
@@ -8,28 +10,37 @@ namespace GovUk.Frontend.AspNetCore.TagHelpers
     [HtmlTargetElement("govuk-back-link", TagStructure = TagStructure.NormalOrSelfClosing)]
     public class BackLinkTagHelper : LinkTagHelperBase
     {
+        private const string AttributesPrefix = "back-link-";
+
         public BackLinkTagHelper(IGovUkHtmlGenerator htmlGenerator, IUrlHelperFactory urlHelperFactory)
             : base(htmlGenerator, urlHelperFactory)
         {
         }
 
-        public override void Process(TagHelperContext context, TagHelperOutput output)
-        {
-            var originalTagMode = output.TagMode;
+        [HtmlAttributeName(DictionaryAttributePrefix = AttributesPrefix)]
+        public IDictionary<string, string> Attributes { get; set; }
 
-            var tagBuilder = CreateAnchorTagBuilder();
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.ThrowIfOutputHasAttributes();
+
+            IHtmlContent content = new HtmlString("Back");
+
+            if (output.TagMode == TagMode.StartTagAndEndTag)
+            {
+                content = await output.GetChildContentAsync();
+            }
+
+            var href = ResolveHref();
+
+            var tagBuilder = Generator.GenerateBackLink(href, Attributes, content);
 
             output.TagName = tagBuilder.TagName;
             output.TagMode = TagMode.StartTagAndEndTag;
 
-            if (originalTagMode == TagMode.SelfClosing)
-            {
-                output.Content.Append("Back");
-            }
-
             output.Attributes.Clear();
             output.MergeAttributes(tagBuilder);
-            output.AddClass("govuk-back-link", HtmlEncoder.Default);
+            output.Content.SetHtmlContent(tagBuilder.InnerHtml);
         }
     }
 }
