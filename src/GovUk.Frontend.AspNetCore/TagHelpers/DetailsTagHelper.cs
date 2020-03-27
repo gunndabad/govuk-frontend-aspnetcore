71dfbdf31423c34202ee54ca9e8b41a280e7eb0d
James Gunn <james@gunn.io>
2020-03-27 16:09:05 +0000

    Assign property

--- src/GovUk.Frontend.AspNetCore/TagHelpers/DetailsTagHelper.cs
+++ src/GovUk.Frontend.AspNetCore/TagHelpers/DetailsTagHelper.cs
@@ -23,7 +23,7 @@ namespace GovUk.Frontend.AspNetCore.TagHelpers
         }
 
         [HtmlAttributeName(DictionaryAttributePrefix = AttributesPrefix)]
-        public IDictionary<string, string> Attributes { get; set; }
+        public IDictionary<string, string> Attributes { get; set; } = new Dictionary<string, string>();
 
         [HtmlAttributeName(IdAttributeName)]
         public string Id { get; set; }
