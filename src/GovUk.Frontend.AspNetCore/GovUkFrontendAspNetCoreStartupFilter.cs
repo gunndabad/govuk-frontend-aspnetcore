6059f8705e2791307355fd8df34b781dc2414c62
James Gunn <james@gunn.io>
2020-10-29 20:34:11 +0000

    Use a startup filter for adding the static files to the app

---
+++ src/GovUk.Frontend.AspNetCore/GovUkFrontendAspNetCoreStartupFilter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.FileProviders;
+
+namespace GovUk.Frontend.AspNetCore
+{
+    public class GovUkFrontendAspNetCoreStartupFilter : IStartupFilter
+    {
+        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+        {
+            if (next == null)
+            {
+                throw new ArgumentNullException(nameof(next));
+            }
+
+            return app =>
+            {
+                app.UseStaticFiles(new StaticFileOptions()
+                {
+                    FileProvider = new ManifestEmbeddedFileProvider(
+                      typeof(ApplicationBuilderExtensions).Assembly,
+                      root: "Content"),
+                    RequestPath = ""  // important - CSS assumes things are available at the root
+                });
+
+                next(app);
+            };
+        }
+    }
+}
