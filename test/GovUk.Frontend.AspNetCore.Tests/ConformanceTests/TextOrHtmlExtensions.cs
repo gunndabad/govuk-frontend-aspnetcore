b0c4ca6f905018c876b8c0a5e57cff5d08f84746
James Gunn <james@gunn.io>
2020-12-09 19:01:13 +0000

    Add ITextOrHtmlContent and method for resolving IHtmlContent from it
    The GDS nunjucks templates use an html/text attribute pair wherever
    custom content needs to be plugged in. This is a convenience mechanism
    for resolving content given are pair of those attributes.

---
+++ test/GovUk.Frontend.AspNetCore.Tests/ConformanceTests/TextOrHtmlExtensions.cs
@@ -0,0 +1,25 @@
+using System.Text.Encodings.Web;
+using GovUk.Frontend.AspNetCore.Tests.ConformanceTests.OptionsJson;
+using Microsoft.AspNetCore.Html;
+
+namespace GovUk.Frontend.AspNetCore.Tests.ConformanceTests
+{
+    public static class TextOrHtmlExtensions
+    {
+        public static IHtmlContent GetHtmlContent(this ITextOrHtmlContent textOrHtml)
+        {
+            if (textOrHtml.Html != null)
+            {
+                return new HtmlString(textOrHtml.Html);
+            }
+            else if (textOrHtml.Text != null)
+            {
+                return new HtmlString(HtmlEncoder.Default.Encode(textOrHtml.Text));
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
