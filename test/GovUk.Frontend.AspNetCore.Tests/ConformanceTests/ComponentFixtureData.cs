6de7bbddd26e44bbd6b3e032b06432b82d139938
James Gunn <james@gunn.io>
2020-12-09 17:19:06 +0000

    Add custom xUnit DataAttribute that looks at fixtures.json
    Given a file name, find the corresponding fixtures.json file. Parse it
    and return a data set for every item in the collection, converting the
    options bag to the specified .NET type.

---
+++ test/GovUk.Frontend.AspNetCore.Tests/ConformanceTests/ComponentFixtureData.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json.Linq;
+using Xunit.Sdk;
+
+namespace GovUk.Frontend.AspNetCore.Tests.ConformanceTests
+{
+    public class ComponentFixtureData : DataAttribute
+    {
+        private readonly string _fixtureFolder;
+        private readonly Type _optionsType;
+        private readonly HashSet<string> _exclude;
+
+        public ComponentFixtureData(
+            string fixtureFolder,
+            Type optionsType,
+            params string[] exclude)
+        {
+            _fixtureFolder = fixtureFolder ?? throw new ArgumentNullException(nameof(fixtureFolder));
+            _optionsType = optionsType ?? throw new ArgumentNullException(nameof(optionsType));
+            _exclude = new HashSet<string>(exclude ?? Array.Empty<string>());
+        }
+
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            var fixturesFile = Path.Combine("ConformanceTests", "Fixtures", _fixtureFolder, "fixtures.json");
+
+            if (!File.Exists(fixturesFile))
+            {
+                throw new FileNotFoundException(
+                    $"Could not find fixtures file at: '{fixturesFile}'.",
+                    fixturesFile);
+            }
+
+            var fixturesJson = File.ReadAllText(fixturesFile);
+            var fixtures = JObject.Parse(fixturesJson).SelectToken("fixtures");
+
+            if (fixtures == null)
+            {
+                throw new InvalidOperationException($"Couldn't find fixtures in '{fixturesFile}'.");
+            }
+
+            foreach (var fixture in fixtures)
+            {
+                var name = fixture["name"].ToString();
+
+                if (_exclude.Contains(name))
+                {
+                    continue;
+                }
+
+                var options = fixture["options"].ToObject(_optionsType);
+                var html = fixture["html"].ToString();
+
+                yield return new object[]
+                {
+                    name,
+                    options,
+                    html
+                };
+            }
+        }
+    }
+}
