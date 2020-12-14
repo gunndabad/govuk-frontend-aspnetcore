ab82c83f6d9b4a9810713e4fe1a8c1f245e40134
James Gunn <james@gunn.io>
2020-12-14 16:28:40 +0000

    v1.0 Back link

---
+++ test/GovUk.Frontend.AspNetCore.ConformanceTests/OptionsJson/BackLink.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace GovUk.Frontend.AspNetCore.ConformanceTests.OptionsJson
+{
+    public class BackLink : ITextOrHtmlContent
+    {
+        public string Html { get; set; }
+        public string Text { get; set; }
+        public string Href { get; set; }
+        public string Classes { get; set; }
+        public IDictionary<string, string> Attributes { get; set; }
+    }
+}
