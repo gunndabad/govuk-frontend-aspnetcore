fa9a7e71b77fd7185fda73222623cbafc2b17b0b
James Gunn <james@gunn.io>
2020-12-14 17:33:03 +0000

    Add a dummy controller endpoint for samples of URL-generating tag helpers

---
+++ src/GovUk.Frontend.AspNetCore.DocSamples/StubControllers/HomeController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GovUk.Frontend.AspNetCore.DocSamples.StubControllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index() => Ok();
+    }
+}
