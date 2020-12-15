d95139a95d0be3c68bb84b1b7933406df194fc3e
James Gunn <james@gunn.io>
2020-12-15 19:59:37 +0000

    Add Button conformance tests

---
+++ test/GovUk.Frontend.AspNetCore.ConformanceTests/RazorGenerator.Button.cs
@@ -0,0 +1,62 @@
+using GovUk.Frontend.AspNetCore.ConformanceTests.OptionsJson;
+using GovUk.Frontend.AspNetCore.TestCommon;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace GovUk.Frontend.AspNetCore.ConformanceTests
+{
+    public partial class RazorGenerator
+    {
+        public string GenerateButton(Button options)
+        {
+            var button = new TagBuilder("govuk-button");
+
+            button.AddCssClass(options.Classes);
+            button.MergeAttributes(options.Attributes);
+
+            if (options.Element == "a" && options.Href == null)
+            {
+                options.Href = "#";
+            }
+
+            if (options.Name != null && options.Href == null)
+            {
+                button.Attributes.Add("name", options.Name);
+            }
+
+            if (options.Type != null)
+            {
+                button.Attributes.Add("type", options.Type);
+            }
+
+            if (options.Value != null)
+            {
+                button.Attributes.Add("value", options.Value);
+            }
+
+            if (options.Disabled.HasValue)
+            {
+                button.Attributes.Add("disabled", options.Disabled.Value ? "true" : "false");
+            }
+
+            if (options.Href != null)
+            {
+                button.Attributes.Add("href", options.Href);
+            }
+
+            if (options.PreventDoubleClick.HasValue)
+            {
+                button.Attributes.Add("prevent-double-click", options.PreventDoubleClick.Value ? "true" : "false");
+            }
+
+            if (options.IsStartButton.HasValue)
+            {
+                button.Attributes.Add("is-start-button", options.IsStartButton.Value ? "true" : "false");
+            }
+
+            var content = options.GetHtmlContent();
+            button.InnerHtml.AppendHtml(content);
+
+            return button.RenderToString();
+        }
+    }
+}
