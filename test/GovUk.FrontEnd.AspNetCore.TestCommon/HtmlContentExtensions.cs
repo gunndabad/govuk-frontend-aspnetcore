36a2782077382af1aa4940982be02d9655741855
James Gunn <james@gunn.io>
2020-12-16 11:28:50 +0000

    Add helper method for getting an AngleSharp IElement from IHtmlContent

--- test/GovUk.FrontEnd.AspNetCore.TestCommon/HtmlContentExtensions.cs
+++ test/GovUk.FrontEnd.AspNetCore.TestCommon/HtmlContentExtensions.cs
@@ -1,11 +1,23 @@
 using System.IO;
 using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Dom;
 using Microsoft.AspNetCore.Html;
 
 namespace GovUk.Frontend.AspNetCore.TestCommon
 {
     public static class HtmlContentExtensions
     {
+        public static async Task<IElement> RenderToElement(this IHtmlContent content)
+        {
+            var html = content.RenderToString();
+
+            var browsingContext = BrowsingContext.New();
+            var doc = await browsingContext.OpenAsync(req => req.Content(html));
+            return doc.Body.FirstElementChild;
+        }
+
         public static string RenderToString(this IHtmlContent content)
         {
             using var writer = new StringWriter();
