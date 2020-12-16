6814576e8dadb432483dc94c74e726543f9d881b
James Gunn <james@gunn.io>
2020-12-16 18:05:53 +0000

    v1.0 Button polish
    Split ButtonTagHelper into two; one that generates button elements and
    another that generates links ('a' tags). This is so we can accurately
    add OutputElementHint attributes and get better intellisense, which is
    very useful for this particular component.
    
    Removed some attributes that can now just be passed through directly
    (now that intellisense is improved by the above change) - type, name,
    value etc.
    
    Also removed the use of LinkTagHelperBase. Turns out we can extend the
    built-in tag helpers that do link generation for our own elements and
    remove a tonne of complexity.

---
+++ src/GovUk.Frontend.AspNetCore/TagHelpers/AnchorTagHelper.cs
@@ -0,0 +1,29 @@
+#nullable enable
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace GovUk.Frontend.AspNetCore.TagHelpers
+{
+    /// <summary>
+    /// <see cref="ITagHelper"/> implementation targeting &lt;a&gt; elements.
+    /// </summary>
+    [HtmlTargetElement("govuk-button-link", Attributes = "asp-action")]
+    [HtmlTargetElement("govuk-button-link", Attributes = "asp-controller")]
+    [HtmlTargetElement("govuk-button-link", Attributes = "asp-area")]
+    [HtmlTargetElement("govuk-button-link", Attributes = "asp-page")]
+    [HtmlTargetElement("govuk-button-link", Attributes = "asp-page-handler")]
+    [HtmlTargetElement("govuk-button-link", Attributes = "asp-fragment")]
+    [HtmlTargetElement("govuk-button-link", Attributes = "asp-host")]
+    [HtmlTargetElement("govuk-button-link", Attributes = "asp-protocol")]
+    [HtmlTargetElement("govuk-button-link", Attributes = "asp-route")]
+    [HtmlTargetElement("govuk-button-link", Attributes = "asp-all-route-data")]
+    [HtmlTargetElement("govuk-button-link", Attributes = "asp-route-*")]
+    public class AnchorTagHelper : Microsoft.AspNetCore.Mvc.TagHelpers.AnchorTagHelper
+    {
+        /// <inheritdoc/>
+        public AnchorTagHelper(IHtmlGenerator generator)
+            : base(generator)
+        {
+        }
+    }
+}
