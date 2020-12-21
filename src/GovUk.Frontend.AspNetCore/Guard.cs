80c4cd48374cba0df43faadb5e738f77a526234b
James Gunn <james@gunn.io>
2020-12-21 14:11:28 +0000

    Add helper class for argument checking & throwing

---
+++ src/GovUk.Frontend.AspNetCore/Guard.cs
@@ -0,0 +1,64 @@
+#nullable enable
+using System;
+using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GovUk.Frontend.AspNetCore
+{
+    internal static class Guard
+    {
+        public static T ArgumentNotNull<T>(string argName, [NotNull] T? argValue)
+            where T : class
+        {
+            if (argValue == null)
+            {
+                throw new ArgumentNullException(argName);
+            }
+
+            return argValue;
+        }
+
+        public static string ArgumentNotNullOrEmpty(string argName, [NotNull] string? argValue)
+        {
+            if (argValue == null)
+            {
+                throw new ArgumentNullException(argName);
+            }
+
+            if (string.IsNullOrEmpty(argValue))
+            {
+                throw new ArgumentException("Argument was empty.", argName);
+            }
+
+            return argValue;
+        }
+
+        public static T ArgumentNotNullOrEmpty<T>(string argName, [NotNull] T? argValue)
+            where T : class, IEnumerable
+        {
+            ArgumentNotNull(argName, argValue);
+
+            if (!argValue.GetEnumerator().MoveNext())
+            {
+                throw new ArgumentException("Argument was empty.", argName);
+            }
+
+            return argValue;
+        }
+
+        public static T ArgumentValidNotNull<T>(
+            string argName,
+            string message,
+            [NotNull] T? testValue,
+            bool test)
+            where T : class
+        {
+            if (testValue == null || !test)
+            {
+                throw new ArgumentException(message, argName);
+            }
+
+            return testValue;
+        }
+    }
+}
