7f46bcb546af14ee18cca706f4182e17d4ee4e5d
James Gunn <james@gunn.io>
2020-12-24 18:56:26 +0000

    v1.0 Hint
    Tag helper is removed; it's not meant to be used directly.
    
    Added conformance tests.

---
+++ test/GovUk.Frontend.AspNetCore.ConformanceTests/OptionsJson/Hint.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace GovUk.Frontend.AspNetCore.ConformanceTests.OptionsJson
+{
+    public class Hint
+    {
+        public string Text { get; set; }
+        public string Html { get; set; }
+        public string Id { get; set; }
+        public string Classes { get; set; }
+        public IDictionary<string, object> Attributes { get; set; }
+    }
+}
