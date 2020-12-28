5e67c11919b5d8ba5493767a66b56fce746373c0
James Gunn <james@gunn.io>
2020-12-28 15:51:08 +0000

    v1.0 Phase banner polish

---
+++ src/GovUk.Frontend.AspNetCore/TagHelpers/PhaseBannerContext.cs
@@ -0,0 +1,32 @@
+#nullable enable
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Html;
+
+namespace GovUk.Frontend.AspNetCore.TagHelpers
+{
+    internal class PhaseBannerContext
+    {
+        public (IDictionary<string, string>? Attributes, IHtmlContent Content)? Tag { get; private set; }
+
+        public void SetTag(IDictionary<string, string>? attributes, IHtmlContent content)
+        {
+            Guard.ArgumentNotNull(nameof(content), content);
+
+            if (Tag != null)
+            {
+                throw ExceptionHelper.OnlyOneElementIsPermittedIn(
+                    PhaseBannerTagTagHelper.TagName, PhaseBannerTagHelper.TagName);
+            }
+
+            Tag = (attributes, content);
+        }
+
+        public void ThrowIfIncomplete()
+        {
+            if (Tag == null)
+            {
+                throw ExceptionHelper.AChildElementMustBeProvided(PhaseBannerTagTagHelper.TagName);
+            }
+        }
+    }
+}
