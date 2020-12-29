de8fd75f1f558c5968f918e44e180cd4d80b1d48
James Gunn <james@gunn.io>
2020-12-29 14:28:43 +0000

    Add helper method for generating form group components

--- test/GovUk.Frontend.AspNetCore.ConformanceTests/ComponentTests.cs
+++ test/GovUk.Frontend.AspNetCore.ConformanceTests/ComponentTests.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using AngleSharp.Diffing.Core;
+using GovUk.Frontend.AspNetCore.ConformanceTests.OptionsJson;
 using GovUk.Frontend.AspNetCore.HtmlGeneration;
 using GovUk.Frontend.AspNetCore.TestCommon;
 using Microsoft.AspNetCore.Html;
@@ -8,17 +10,90 @@ namespace GovUk.Frontend.AspNetCore.ConformanceTests
 {
     public partial class ComponentTests
     {
+        private delegate IHtmlContent GenerateFormGroupElement(bool haveError, string describedBy);
+
         private static readonly IHtmlContent _emptyContent = new HtmlString("");
+
+        private readonly ComponentGenerator _componentGenerator;
+
+        public ComponentTests()
+        {
+            _componentGenerator = new ComponentGenerator();
+        }
+
+        private static void AppendToDescribedBy(ref string describedBy, string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return;
+            }
+
+            if (string.IsNullOrEmpty(describedBy))
+            {
+                describedBy = value;
+            }
+            else
+            {
+                describedBy += " " + value;
+            }
+        }
 
         private void CheckComponentHtmlMatchesExpectedHtml<TOptions>(
             ComponentTestCaseData<TOptions> testCaseData,
             Func<ComponentGenerator, TOptions, string> generateComponent,
             Predicate<IDiff> excludeDiff = null)
         {
-            var componentGenerator = new ComponentGenerator();
-            var html = generateComponent(componentGenerator, testCaseData.Options);
+            var html = generateComponent(_componentGenerator, testCaseData.Options);
 
             AssertEx.HtmlEqual(testCaseData.ExpectedHtml, html, excludeDiff);
+        }
+
+        private string GenerateFormGroup(
+            Label label,
+            Hint hint,
+            ErrorMessage errorMessage,
+            FormGroup formGroup,
+            GenerateFormGroupElement generateElement)
+        {
+            var haveError = errorMessage != null;
+
+            string describedBy = null;
+
+            var attributes = new Dictionary<string, string>()
+                .MergeAttribute("class", formGroup?.Classes);
+
+            var contentBuilder = new HtmlContentBuilder();
+
+            if (label != null)
+            {
+                var labelTagBuilder = BuildLabel(_componentGenerator, label);
+                contentBuilder.AppendHtml(labelTagBuilder);
+            }
+
+            if (hint != null)
+            {
+                var hintTagBuilder = BuildHint(_componentGenerator, hint);
+                contentBuilder.AppendHtml(hintTagBuilder);
+
+                AppendToDescribedBy(ref describedBy, hint.Id);
+            }
+
+            if (errorMessage != null)
+            {
+                var errorMessageTagBuilder = BuildErrorMessage(_componentGenerator, errorMessage);
+                contentBuilder.AppendHtml(errorMessageTagBuilder);
+
+                AppendToDescribedBy(ref describedBy, errorMessage.Id);
+            }
+
+            var element = generateElement(haveError, describedBy);
+            contentBuilder.AppendHtml(element);
+
+            return _componentGenerator.GenerateFormGroup(
+                    haveError,
+                    contentBuilder,
+                    attributes)
+                .RenderToString();
         }
     }
 }
