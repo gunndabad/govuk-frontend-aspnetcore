c679f21a741ba63ddffd269486cc3147ab1a4c2a
James Gunn <james@gunn.io>
2021-01-01 16:36:25 +0000

    Update to GOV.UK Frontend v3.10.2

--- src/GovUk.Frontend.AspNetCore/HtmlImports.cs
+++ src/GovUk.Frontend.AspNetCore/HtmlImports.cs
@@ -2,20 +2,20 @@ namespace GovUk.Frontend.AspNetCore
 {
     public static class HtmlSnippets
     {
-        public const string GdsLibraryVersion = "3.10.1";
+        public const string GdsLibraryVersion = "3.10.2";
 
         public const string BodyInitScript = @"<script>
     document.body.className = document.body.className + ' js-enabled';
 </script>";
 
-        public const string ScriptImports = @"<script src=""/govuk-frontend-3.10.1.min.js""></script>
+        public const string ScriptImports = @"<script src=""/govuk-frontend-3.10.2.min.js""></script>
 <script>window.GOVUKFrontend.initAll()</script>";
 
         public const string StyleImports = @"<!--[if !IE 8]><!-->
-    <link rel=""stylesheet"" href=""/govuk-frontend-3.10.1.min.css"">
+    <link rel=""stylesheet"" href=""/govuk-frontend-3.10.2.min.css"">
 <!--<![endif]-->
 <!--[if IE 8]>
-    <link rel = ""stylesheet"" href=""/govuk-frontend-ie8-3.10.1.min.css"">
+    <link rel = ""stylesheet"" href=""/govuk-frontend-ie8-3.10.2.min.css"">
 <![endif]-->";
     }
 }
