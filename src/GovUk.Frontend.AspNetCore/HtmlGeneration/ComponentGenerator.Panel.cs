cad7f323a26e0549852caa5aa78395f122c74997
James Gunn <james@gunn.io>
2021-06-15 12:38:43 +0100

    V1.0 Panel (#215)
    * Add Panel conformance tests
    
    * v1.0 Panel polish

---
+++ src/GovUk.Frontend.AspNetCore/HtmlGeneration/ComponentGenerator.Panel.cs
@@ -0,0 +1,52 @@
+#nullable enable
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace GovUk.Frontend.AspNetCore.HtmlGeneration
+{
+    public partial class ComponentGenerator
+    {
+        internal const int PanelDefaultHeadingLevel = 1;
+        internal const string PanelElement = "div";
+        internal const int PanelMinHeadingLevel = 1;
+        internal const int PanelMaxHeadingLevel = 6;
+
+        public TagBuilder GeneratePanel(
+            int headingLevel,
+            IHtmlContent titleContent,
+            IHtmlContent? bodyContent,
+            IDictionary<string, string>? attributes)
+        {
+            if (headingLevel < PanelMinHeadingLevel || headingLevel > PanelMaxHeadingLevel)
+            {
+                throw new ArgumentOutOfRangeException(
+                    $"{nameof(headingLevel)} must be between {PanelMinHeadingLevel} and {PanelMaxHeadingLevel}.",
+                    nameof(headingLevel));
+            }
+
+            Guard.ArgumentNotNull(nameof(titleContent), titleContent);
+
+            var tagBuilder = new TagBuilder(PanelElement);
+            tagBuilder.MergeAttributes(attributes);
+            tagBuilder.MergeCssClass("govuk-panel");
+            tagBuilder.MergeCssClass("govuk-panel--confirmation");
+
+            var heading = new TagBuilder($"h{headingLevel}");
+            heading.MergeCssClass("govuk-panel__title");
+            heading.InnerHtml.AppendHtml(titleContent);
+            tagBuilder.InnerHtml.AppendHtml(heading);
+
+            if (bodyContent != null)
+            {
+                var body = new TagBuilder("div");
+                body.MergeCssClass("govuk-panel__body");
+                body.InnerHtml.AppendHtml(bodyContent);
+                tagBuilder.InnerHtml.AppendHtml(body);
+            }
+
+            return tagBuilder;
+        }
+    }
+}
