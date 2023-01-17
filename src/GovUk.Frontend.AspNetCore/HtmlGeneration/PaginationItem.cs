b2ffbd28765978e2b37aec57338a8be3f1dc9bca
James Gunn <james@gunn.io>
2023-01-17 16:15:49 +0000

    Pagination component (#242)
    * Add generator and conformance tests for Pagination component
    
    * Add Pagination component tag helpers

---
+++ src/GovUk.Frontend.AspNetCore/HtmlGeneration/PaginationItem.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace GovUk.Frontend.AspNetCore.HtmlGeneration
+{
+    internal abstract class PaginationItemBase
+    {
+        private protected PaginationItemBase() { }
+
+        public AttributeDictionary? Attributes { get; set; }
+    }
+
+    internal class PaginationItem : PaginationItemBase
+    {
+        [DisallowNull]
+        public IHtmlContent? Number { get; set; }
+        public string? Href { get; set; }
+        public bool IsCurrent { get; set; }
+        public string? VisuallyHiddenText { get; set; }
+    }
+
+    internal class PaginationItemEllipsis : PaginationItemBase
+    {
+    }
+}
