fc50572987869d6b6e07845f0127dff51eece1ae
James Gunn <james@gunn.io>
2023-10-31 13:28:38 +0000

    Remove the Tag Helper Component that adds style and script imports
    With PageTemplateHelper and the _GovUkPageTemplate.cshtml view I don't
    think this is required any more.

--- test/GovUk.Frontend.AspNetCore.IntegrationTests/StartupFilterTests.cs
+++ test/GovUk.Frontend.AspNetCore.IntegrationTests/StartupFilterTests.cs
@@ -1,9 +1,7 @@
 using System;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using GovUk.Frontend.AspNetCore.TestCommon;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
@@ -57,105 +55,6 @@ public class StartupFilterTests
 
         // Assert
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-    }
-
-    [Fact]
-    public async Task AddImportsToHtmlIsFalse_DoesNotAddStyleAndScriptImportsToRazorViews()
-    {
-        // Arrange
-        // Arrange
-        await using var fixture = new StartupFilterTestFixture(services =>
-        {
-            services.Configure<GovUkFrontendAspNetCoreOptions>(options =>
-            {
-                options.AddImportsToHtml = false;
-                options.CompiledContentPath = "/govuk";
-            });
-        });
-        await fixture.InitializeAsync();
-
-        var request = new HttpRequestMessage(HttpMethod.Get, "/Empty");
-
-        // Act
-        var response = await fixture.HttpClient.SendAsync(request);
-
-        // Assert
-        response.EnsureSuccessStatusCode();
-
-        var html = await response.GetHtmlDocument();
-
-        var head = html.QuerySelector("head");
-
-        var linkTags = head.QuerySelectorAll("link");
-
-        Assert.DoesNotContain(
-            "/govuk/all.min.css",
-            linkTags
-                .Where(t => t.GetAttribute("rel") == "stylesheet")
-                .Select(t => t.GetAttribute("href")));
-
-        var body = html.QuerySelector("body");
-
-        Assert.DoesNotContain("govuk-template__body", body.ClassList);
-
-        var bodyScriptTags = body.QuerySelectorAll("script");
-
-        Assert.DoesNotContain(
-            "/govuk/all.min.js",
-            bodyScriptTags.Select(t => t.GetAttribute("src")));
-
-        Assert.DoesNotContain(
-            $"document.body.className += ' js-enabled' + ('noModule' in HTMLScriptElement.prototype ? ' govuk-frontend-supported' : '');",
-            bodyScriptTags.Select(t => t.InnerHtml));
-    }
-
-    [Fact]
-    public async Task AddImportsToHtmlIsTrue_AddsStyleAndScriptImportsToRazorViews()
-    {
-        // Arrange
-        await using var fixture = new StartupFilterTestFixture(services =>
-        {
-            services.Configure<GovUkFrontendAspNetCoreOptions>(options =>
-            {
-                options.AddImportsToHtml = true;
-                options.CompiledContentPath = "/govuk";
-            });
-        });
-        await fixture.InitializeAsync();
-
-        var request = new HttpRequestMessage(HttpMethod.Get, "/Empty");
-
-        // Act
-        var response = await fixture.HttpClient.SendAsync(request);
-
-        // Assert
-        response.EnsureSuccessStatusCode();
-
-        var html = await response.GetHtmlDocument();
-
-        var head = html.QuerySelector("head");
-
-        var linkTags = head.QuerySelectorAll("link");
-
-        Assert.Contains(
-            "/govuk/all.min.css",
-            linkTags
-                .Where(t => t.GetAttribute("rel") == "stylesheet")
-                .Select(t => t.GetAttribute("href")));
-
-        var body = html.QuerySelector("body");
-
-        Assert.Contains("govuk-template__body", body.ClassList);
-
-        var bodyScriptTags = body.QuerySelectorAll("script");
-
-        Assert.Contains(
-            "/govuk/all.min.js",
-            bodyScriptTags.Select(t => t.GetAttribute("src")));
-
-        Assert.Contains(
-            $"document.body.className += ' js-enabled' + ('noModule' in HTMLScriptElement.prototype ? ' govuk-frontend-supported' : '');",
-            bodyScriptTags.Select(t => t.InnerHtml));
     }
 }
 
