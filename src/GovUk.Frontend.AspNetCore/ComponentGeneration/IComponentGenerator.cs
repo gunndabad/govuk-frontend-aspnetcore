21b5f7ff802fa4a99507b447ce70721e47b9d8c8
James Gunn <james@gunn.io>
2023-10-31 18:12:59 +0000

    Port Back link to IComponentGenerator

--- src/GovUk.Frontend.AspNetCore/ComponentGeneration/IComponentGenerator.cs
+++ src/GovUk.Frontend.AspNetCore/ComponentGeneration/IComponentGenerator.cs
@@ -7,4 +7,9 @@ namespace GovUk.Frontend.AspNetCore.ComponentGeneration;
 /// </summary>
 public interface IComponentGenerator
 {
+    /// <summary>
+    /// Generates a back link component.
+    /// </summary>
+    /// <returns>An <see cref="HtmlTag"/> with the component's HTML.</returns>
+    HtmlTag GenerateBackLink(BackLinkOptions options);
 }
