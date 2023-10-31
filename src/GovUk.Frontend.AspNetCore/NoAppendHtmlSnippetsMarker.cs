cd95410c71a8ec1e08597884a9bd517c1c2a6179
James Gunn <james@gunn.io>
2023-10-31 13:28:38 +0000

    Remove the Tag Helper Component that adds style and script imports
    With PageTemplateHelper and the _GovUkPageTemplate.cshtml view I don't
    think this is required any more.

--- src/GovUk.Frontend.AspNetCore/NoAppendHtmlSnippetsMarker.cs
+++
@@ -1,8 +0,0 @@
-namespace GovUk.Frontend.AspNetCore;
-
-internal sealed class NoAppendHtmlSnippetsMarker
-{
-    public const string ViewDataKey = "GovUk.Frontend.AspNetCore.NoAppendHtmlSnippetsMarker";
-
-    public static NoAppendHtmlSnippetsMarker Instance { get; } = new();
-}
