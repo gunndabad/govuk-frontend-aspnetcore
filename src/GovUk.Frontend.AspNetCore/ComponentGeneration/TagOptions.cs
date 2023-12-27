698a51a4f778df83804281adf0e1732e98e31252
James Gunn <james@gunn.io>
2023-12-27 17:14:39 +0000

    Port Tag to IComponentGenerator

--- src/GovUk.Frontend.AspNetCore/ComponentGeneration/TagOptions.cs
+++ src/GovUk.Frontend.AspNetCore/ComponentGeneration/TagOptions.cs
@@ -10,4 +10,12 @@ public class TagOptions
     public string? Html { get; set; }
     public string? Classes { get; set; }
     public IReadOnlyDictionary<string, string?>? Attributes { get; set; }
+
+    internal void Validate()
+    {
+        if (Html.NormalizeEmptyString() is null && Text.NormalizeEmptyString() is null)
+        {
+            throw new InvalidOptionsException(GetType(), $"{nameof(Html)} or {nameof(Text)} must be specified.");
+        }
+    }
 }
