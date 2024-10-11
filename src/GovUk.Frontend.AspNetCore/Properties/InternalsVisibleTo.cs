f55fa7595d23a10c3c39fe664def771d27cfa4b0
James Gunn <james@gunn.io>
2024-10-11 18:01:27 +0100

    Move InternalsVisibleTo into project file

--- src/GovUk.Frontend.AspNetCore/Properties/InternalsVisibleTo.cs
+++
@@ -1,6 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("GovUk.Frontend.AspNetCore.Tests")]
-[assembly: InternalsVisibleTo("GovUk.Frontend.AspNetCore.ConformanceTests")]
-[assembly: InternalsVisibleTo("GovUk.Frontend.AspNetCore.IntegrationTests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
