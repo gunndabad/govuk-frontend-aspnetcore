b556bd31b90a7c6909d3acf3a38e2c82ff27b122
James Gunn <james@gunn.io>
2024-12-22 14:50:46 +0000

    Add new set of types for creating a component

---
+++ src/GovUk.Frontend.AspNetCore/ComponentGeneration/EncodedAttributesDictionaryBuilder.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Html;
+
+namespace GovUk.Frontend.AspNetCore.ComponentGeneration;
+
+internal class EncodedAttributesDictionaryBuilder
+{
+    private readonly EncodedAttributesDictionary _dictionary;
+
+    public EncodedAttributesDictionaryBuilder()
+        : this(new())
+    {
+    }
+
+    public EncodedAttributesDictionaryBuilder(EncodedAttributesDictionary dictionary)
+    {
+        ArgumentNullException.ThrowIfNull(dictionary);
+
+        _dictionary = dictionary;
+    }
+
+    public EncodedAttributesDictionaryBuilder WithBoolean(string name)
+    {
+        _dictionary.AddBoolean(name);
+
+        return this;
+    }
+
+    public EncodedAttributesDictionaryBuilder WithOther(EncodedAttributesDictionary other)
+    {
+        _dictionary.Add(other);
+
+        return this;
+    }
+
+    public EncodedAttributesDictionaryBuilder With(string name, IHtmlContent value)
+    {
+        _dictionary.Add(name, value);
+
+        return this;
+    }
+
+    public EncodedAttributesDictionaryBuilder With(string name, string value, bool encodeValue)
+    {
+        _dictionary.Add(name, value, encodeValue);
+
+        return this;
+    }
+
+    public EncodedAttributesDictionaryBuilder WithCssClass(string className)
+    {
+        _dictionary.AddCssClass(className);
+
+        return this;
+    }
+
+    public EncodedAttributesDictionaryBuilder WithCssClasses(IEnumerable<string> classNames)
+    {
+        _dictionary.AddCssClasses(classNames);
+
+        return this;
+    }
+
+    internal EncodedAttributesDictionaryBuilder When(Func<bool> condition, Action<EncodedAttributesDictionaryBuilder> action)
+    {
+        if (condition())
+        {
+            action(this);
+        }
+
+        return this;
+    }
+
+    internal EncodedAttributesDictionaryBuilder When(bool condition, Action<EncodedAttributesDictionaryBuilder> action) =>
+        When(() => condition, action);
+
+    internal EncodedAttributesDictionaryBuilder WhenNotNull<T>(Func<T?> getValue, Action<T, EncodedAttributesDictionaryBuilder> action)
+    {
+        var value = getValue();
+        if (value is not null)
+        {
+            action(value, this);
+        }
+
+        return this;
+    }
+
+    internal EncodedAttributesDictionaryBuilder WhenNotNull<T>(T? value, Action<T, EncodedAttributesDictionaryBuilder> action) =>
+        WhenNotNull(() => value, action);
+
+    public static implicit operator EncodedAttributesDictionary(EncodedAttributesDictionaryBuilder builder) => builder._dictionary;
+}
