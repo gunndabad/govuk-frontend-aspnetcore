ab3247ed86039be58b2d0dc0a017d71daa44ac18
James Gunn <james@gunn.io>
2025-05-02 18:24:22 +0100

    Add liquid template for generating a Checkboxes

---
+++ src/GovUk.Frontend.AspNetCore/ComponentGeneration/FieldsetOptions2.cs
@@ -0,0 +1,24 @@
+namespace GovUk.Frontend.AspNetCore.ComponentGeneration;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+public record FieldsetOptions2
+{
+    public string? DescribedBy { get; set; }
+    public FieldsetOptions2Legend? Legend { get; set; }
+    public string? Role { get; set; }
+    public string? Html { get; set; }
+    public string? Classes { get; set; }
+    public AttributeCollection? Attributes { get; set; }
+}
+
+public record FieldsetOptions2Legend
+{
+    public string? Text { get; set; }
+    public string? Html { get; set; }
+    public bool? IsPageHeading { get; set; }
+    public string? Classes { get; set; }
+
+    [NonStandardParameter]
+    public EncodedAttributesDictionary? Attributes { get; set; }
+}
