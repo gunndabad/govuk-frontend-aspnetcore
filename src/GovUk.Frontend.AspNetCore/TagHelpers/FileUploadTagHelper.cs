fc8d91ee8b7f2a9963403a1fba22ea6e3c835740
James Gunn <james@gunn.io>
2025-05-04 15:12:50 +0100

    Add missing diagnostic ID

--- src/GovUk.Frontend.AspNetCore/TagHelpers/FileUploadTagHelper.cs
+++ src/GovUk.Frontend.AspNetCore/TagHelpers/FileUploadTagHelper.cs
@@ -58,7 +58,7 @@ public class FileUploadTagHelper : TagHelper
     /// An expression to be evaluated against the current model.
     /// </summary>
     [HtmlAttributeName(AspForAttributeName)]
-    [Obsolete("Use the 'for' attribute instead.")]
+    [Obsolete("Use the 'for' attribute instead.", DiagnosticId = DiagnosticIds.UseForAttributeInstead)]
     public ModelExpression? AspFor
     {
         get => For;
